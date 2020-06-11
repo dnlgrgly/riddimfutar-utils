1d55755779f3dfa46ac5c821ae8a0d85f0553c39
danielgrgly <danielgrgly@gmail.com>
2020-06-11 15:31:47 +0200

    add axios, init initDb

--- initDb.js
+++ initDb.js
@@ -1,3 +1,8 @@
+const axios = require("axios");
 const mongoose = require("mongoose");
 
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true });
+
+// implement stuff here
+
+mongoose.connection.close();
