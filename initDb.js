3bebdc45e36f8e87e0a576eae5a352c0f5b8974c
danielgrgly <danielgrgly@gmail.com>
2020-06-17 18:12:41 +0200

    add manual matching capabilities

--- initDb.js
+++ initDb.js
@@ -6,7 +6,16 @@ const axios = require("axios");
 const mongoose = require("mongoose");
 const { Storage } = require("@google-cloud/storage");
 
+// overwrite renamed stops with obsolote stop name mp3 filenames
+const overwrites = require("./dataSrc/mp3config.json");
+
 const storage = new Storage();
+
+let unusedFilenames = [];
+let unusedStops = [];
+let results = [];
+let stops = [];
+let files = [];
 
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true });
 
@@ -27,56 +36,20 @@ const MHMH = (stopName) => {
   return res;
 };
 
-const main = async () => {
-  // fetch list of available file names
-  const [files] = await storage.bucket("futar").getFiles();
-
-  files.sort();
-
-  // fetch data from BKK API
-  const bkkRaw = await axios.get(
-    "https://futar.bkk.hu/api/query/v1/ws/otp/api/where/stops-for-location.json?key=riddimfutar-utils"
-  );
-
-  // drop useless data
-  const stops = bkkRaw.data.data.list
-    .map((stopRaw) => {
-      return { name: stopRaw.name, id: stopRaw.id };
-    })
-    // stops with a non-F(UTAR) or CS(oport)F(UTAR) code are usually
-    // underpass entraces and administrative stops
-    // -- we don't need them
-    .filter((stop) => stop.id.includes("BKK_F") || stop.id.includes("BKK_CSF"));
-
-  stops.sort();
-
-  let unusedFilenames = [...files];
-  let unusedStops = [...stops];
-  let results = [];
-
-  for (let i = 0; i < stops.length; i++) {
-    const { name, id } = stops[i];
-    let stopName = normalise(name);
-
-    const fileIndex = files.findIndex((file) =>
-      MHMH(file.name).includes(stopName)
-    );
-
-    if (fileIndex > 0) {
-      results.push({ name, id, fileName: files[fileIndex].name });
-      //   console.log(`✅ ${name} - ${files[fileIndex].name}`);
-      const unusedFileIndex = unusedFilenames.indexOf(files[fileIndex]);
-      if (unusedFileIndex > -1) {
-        unusedFilenames.splice(unusedFileIndex, 1);
-      }
-      const unusedStopIndex = unusedStops.indexOf(stops[i]);
-      if (unusedStopIndex > -1) {
-        unusedStops.splice(unusedStopIndex, 1);
-      }
-    }
+const match = (i, name, id, file) => {
+  results.push({ name, id, fileName: file.name });
+  //   console.log(`✅ ${name} - ${files[fileIndex].name}`);
+  const unusedFileIndex = unusedFilenames.indexOf(file);
+  if (unusedFileIndex > -1) {
+    unusedFilenames.splice(unusedFileIndex, 1);
   }
+  const unusedStopIndex = unusedStops.indexOf(stops[i]);
+  if (unusedStopIndex > -1) {
+    unusedStops.splice(unusedStopIndex, 1);
+  }
+};
 
-  // post-match results
+const printStats = () => {
   console.log(`✅ Matches: ${results.length}`);
 
   if (unusedFilenames.length > 0) {
@@ -88,8 +61,60 @@ const main = async () => {
   if (unusedStops.length > 0) {
     console.log("⚠️ Unmatched stop names ⚠️");
     console.log(`${unusedStops.length} not used (out of ${stops.length})`);
-    // unusedStops.forEach((stop) => console.log(`❌ ${stop.name}`));
+    unusedStops.forEach((stop) => console.log(`❌ ${stop.name}`));
   }
+};
+
+const main = async () => {
+  // fetch list of available file names
+  [files] = await storage.bucket("futar").getFiles();
+
+  files.sort();
+
+  // fetch data from BKK API
+  const bkkRaw = await axios.get(
+    "https://futar.bkk.hu/api/query/v1/ws/otp/api/where/stops-for-location.json?key=riddimfutar-utils"
+  );
+
+  // drop useless data
+  stops = bkkRaw.data.data.list
+    .map((stopRaw) => {
+      return { name: stopRaw.name, id: stopRaw.id };
+    })
+    // stops with a non-F(UTAR) or CS(oport)F(UTAR) code are usually
+    // underpass entraces and administrative stops
+    // -- we don't need them
+    .filter((stop) => stop.id.includes("BKK_F") || stop.id.includes("BKK_CSF"));
+
+  stops.sort();
+
+  unusedFilenames = [...files];
+  unusedStops = [...stops];
+
+  // loop over every stop
+  for (let i = 0; i < stops.length; i++) {
+    const { name, id } = stops[i];
+
+    // if there is an overwrite defined, use that
+    if (overwrites[name]) {
+      match(i, name, id, overwrites[name]);
+    }
+
+    let stopName = normalise(name);
+
+    // check the index of the file that has the same name as the stop in question
+    const fileIndex = files.findIndex((file) =>
+      MHMH(file.name).includes(stopName)
+    );
+
+    // if yes, match the stops
+    if (fileIndex > -1) {
+      match(i, name, id, files[fileIndex]);
+    }
+  }
+
+  // post-matching results
+  printStats();
 };
 
 main();
